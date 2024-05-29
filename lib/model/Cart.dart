2896adc61f16177c72034ee600cfe49bf4dcaeee
JIJ2003 <21130579@st.hcmuaf.edu.vn>
2024-05-29 23:12:32 +0700

    chức năng thêm vào giỏ và phân loại

--- lib/model/Cart.dart
+++ lib/model/Cart.dart
@@ -1,26 +1,31 @@
 import 'package:android/model/Product.dart';
-
+//Nguyen Pham Quoc Tri
 class Cart {
-  final Product product;
-  int quantity;
+  // Thuộc tính của lớp Cart
+  final Product product; // Sản phẩm
+  int quantity; // Số lượng sản phẩm trong giỏ hàng
 
+  // Constructor để khởi tạo một đối tượng Cart
   Cart({required this.product, this.quantity = 1});
 
+  // Factory constructor để tạo một đối tượng Cart từ một đối tượng JSON
   factory Cart.fromJson(Map<String, dynamic> json) {
     return Cart(
-      product: Product.fromJson(json['product']),
-      quantity: json['quantity'],
+      product: Product.fromJson(json['product']), // Chuyển đổi JSON thành đối tượng Product
+      quantity: json['quantity'], // Lấy số lượng từ JSON
     );
   }
 
+  // Phương thức để chuyển đổi một đối tượng Cart thành một đối tượng JSON
   Map<String, dynamic> toJson() {
     return {
-      'product': product.toJson(),
-      'quantity': quantity,
+      'product': product.toJson(), // Chuyển đổi đối tượng Product thành JSON
+      'quantity': quantity, // Số lượng sản phẩm
     };
   }
 
+  // Phương thức lấy tên sản phẩm trong giỏ hàng
   String getProduct(){
-    return this.product.productname;
+    return this.product.productname; // Trả về tên sản phẩm
   }
 }
--- lib/model/Category.dart
+++ lib/model/Category.dart
@@ -1,27 +1,31 @@
 import 'dart:core';
-
+//Nguyen Pham Quoc Tri
 class Category {
+  // Thuộc tính của lớp Category
   int categoryID;
   String name;
   String image;
 
+  // Constructor để khởi tạo một đối tượng Category
   Category({
     required this.categoryID,
     required this.name,
     required this.image,
   });
 
+  // Factory constructor để tạo một đối tượng Category từ một đối tượng JSON
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
-      categoryID: int.parse(json['categoryID']),
-      name: json['name'] as String,
-      image: json['image'] as String,
+      categoryID: int.parse(json['categoryID']), // Chuyển đổi categoryID từ String sang int
+      name: json['name'] as String, // Phân tích name thành String
+      image: json['image'] as String, // Phân tích image thành String
     );
   }
+
+  // Phương thức để chuyển đổi một đối tượng Category thành một đối tượng JSON
   Map<String, dynamic> toJson() => {
-    'categoryID': categoryID.toString(),
+    'categoryID': categoryID.toString(), // Chuyển đổi categoryID từ int sang String
     'name': name,
     'image': image,
-
-  };
+  }
 }
